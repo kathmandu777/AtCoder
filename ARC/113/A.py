ea2d10a38791d8ad73e1d461118e30a879eb1749
kathmandu <mahhakatomanato@gmail.com>
2021-03-14 22:52:38 +0900

    pythonだとダメでpypyだと通る

---
+++ ARC/113/A.py
@@ -0,0 +1,16 @@
+#from icecream import ic
+
+k = int(input())
+
+ans = 0
+for a in range(1, k + 1):
+    for b in range(1, k + 1):
+        if (a * b > k):
+            break
+        for c in range(1, k + 1):
+            #ic(a, b, c)
+            if (a * b * c > k):
+                break
+            else:
+                ans += 1
+print(ans)
